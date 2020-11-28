acdad4ba77088e7e1d027180efb3276e9e7f845d
Sven Becker <ujily@student.kit.edu>
2020-11-28 21:34:51 +0100

    Complete hw-interfaces and introduce dynamic speed scaling and pausing

--- coppeliasim_interface/include/coppeliasim_client.h
+++ coppeliasim_interface/include/coppeliasim_client.h
@@ -5,20 +5,31 @@
 #include <hardware_interface/robot_hw.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/force_torque_sensor_interface.h>
 #include <algorithm>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/String.h>
 #include <std_srvs/Trigger.h>
+#include <std_srvs/SetBool.h>
+#include <std_msgs/Float64MultiArray.h>
 #include <ur_controllers/speed_scaling_interface.h>
 #include <ur_controllers/scaled_joint_command_interface.h>
+#include <realtime_tools/realtime_publisher.h>
+#include "tf2_msgs/TFMessage.h"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "ur_msgs/SetSpeedSliderFraction.h"
+#include <industrial_robot_status_interface/industrial_robot_status_interface.h>
+
 
 
 extern "C" {
    #include "extApi.h"
 }
 
-enum class PausingState
+
+
+enum class RunState
 {
   PAUSED,
   RUNNING,
@@ -41,35 +52,45 @@ class CoppeliaSimInterface : public hardware_interface::RobotHW {
         bool connectToCoppeliaSim();
         bool disconnectFromCoppeliaSim();
         bool advanceCoppeliaSim();
-        // bool startSimulation();
+        void extractToolPose(const ros::Time& timestamp);
+        void transformForceTorque();
+        void publishPose();
+        bool zeroFTSensor(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res);
+        bool setSpeedSlider(ur_msgs::SetSpeedSliderFractionRequest& req, ur_msgs::SetSpeedSliderFractionResponse& res);
+        bool setPause(std_srvs::SetBoolRequest& req, std_srvs::SetBoolResponse& res);
+
     protected:
         bool checkControllerClaims(const std::set<std::string>& claimed_resources);
         
         
     private:
-        simxInt joint_handles_[6];
-        double joint_velocity_command_[6];
-        double joint_position_command_[6];
-        double joint_positions_[6];
-        double joint_efforts_[6];
-        double joint_velocities_[6];
+        std::vector<simxInt> joint_handles_;
+        std::vector<double> joint_velocity_command_;
+        std::vector<double> joint_position_command_;
+        std::vector<double> joint_positions_;
+        std::vector<double> joint_efforts_;
+        std::vector<double> joint_velocities_;
+        std::vector<double> fts_measurements_;
         std::vector<std::string> joint_names_;
+        
         size_t n_joints = 6;
 
+        ros::ServiceServer tare_sensor_srv_;
+        ros::ServiceServer deactivate_srv_;
 
         uint32_t runtime_state_;
         bool position_controller_running_;
         bool velocity_controller_running_;
 
-        PausingState pausing_state_;
+        RunState run_state_;
         double pausing_ramp_up_increment_;
 
         bool controller_reset_necessary_;
         bool controllers_initialized_;
         bool coppeliasim_simulating_;
+        industrial_robot_status_interface::RobotStatus robot_status_resource_{};
+        industrial_robot_status_interface::IndustrialRobotStatusInterface robot_status_interface_{};
 
-        double target_speed_scaling_;
-        double speed_scaling_combined_;
         
         
         hardware_interface::JointStateInterface js_interface_;
@@ -78,10 +99,30 @@ class CoppeliaSimInterface : public hardware_interface::RobotHW {
         ur_controllers::SpeedScalingInterface speedsc_interface_;
         hardware_interface::VelocityJointInterface vj_interface_;
         ur_controllers::ScaledVelocityJointInterface svj_interface_;
+        hardware_interface::ForceTorqueSensorInterface fts_interface_;
 
+        std::unique_ptr<realtime_tools::RealtimePublisher<tf2_msgs::TFMessage>> tcp_pose_pub_;
+
+        ros::Publisher pub;
         ros::NodeHandle root_nh;
         ros::NodeHandle robot_nh;
         simxInt client_id_;
+        std::string tf_prefix_;
+
+        tf2::Vector3 tcp_torque_;
+        tf2::Vector3 tcp_force_;
+        simxInt ft_sensor_hande_;
+        simxInt tcp_handle_;
+        simxInt base_handle_;
+        simxFloat tcp_pose_[7];
+        geometry_msgs::TransformStamped tcp_transform_;
+        double speed_scaling_;
+        double target_speed_fraction_;
+        double speed_scaling_combined_;
+        ros::ServiceServer pause_button_srv_;
+        ros::ServiceServer set_speed_slider_srv_;
+
+        
 
 
        
--- coppeliasim_interface/src/coppeliasim_client.cpp
+++ coppeliasim_interface/src/coppeliasim_client.cpp
@@ -1,9 +1,29 @@
 #include "../include/coppeliasim_client.h"
+#include <pluginlib/class_list_macros.hpp>
+
 
 
 CoppeliaSimInterface::CoppeliaSimInterface(){
     joint_names_.resize(6, "");
-    
+    joint_efforts_.resize(6, 0);
+    joint_handles_.resize(6, -1);
+    joint_position_command_.resize(6, 0);
+    joint_positions_.resize(6, 0);
+    joint_velocities_.resize(6, 0);
+    joint_velocity_command_.resize(6, 0);
+    fts_measurements_.resize(6, 0);
+    pausing_ramp_up_increment_ = 0.01;
+    target_speed_fraction_ = 1;
+    run_state_ = RunState::RUNNING;
+    speed_scaling_ = 1;
+    robot_status_resource_.drives_powered = industrial_robot_status_interface::TriState::TRUE;
+    robot_status_resource_.e_stopped = industrial_robot_status_interface::TriState::FALSE;
+    robot_status_resource_.error_code = 0;
+    robot_status_resource_.in_error = industrial_robot_status_interface::TriState::FALSE;
+    robot_status_resource_.in_motion = industrial_robot_status_interface::TriState::FALSE;
+    robot_status_resource_.mode = industrial_robot_status_interface::RobotMode::AUTO;
+    robot_status_resource_.motion_possible = industrial_robot_status_interface::TriState::FALSE;
+
     
     
     this->client_id_ = -1;
@@ -24,6 +44,8 @@ CoppeliaSimInterface::~CoppeliaSimInterface(){
 bool CoppeliaSimInterface::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_nh){
     this->root_nh = root_nh;
     this->robot_nh = robot_nh;
+    pub = robot_nh.advertise<std_msgs::Float64MultiArray>("tool0FTS", 10, false);
+    std::string wrench_frame_id;
     std::string coppeliasim_ip_;
     simxInt coppeliasim_port_;
     if(! (this->robot_nh.getParam("coppeliasim_ip", coppeliasim_ip_) && this->robot_nh.getParam("coppeliasim_port", coppeliasim_port_))){
@@ -45,10 +67,13 @@ bool CoppeliaSimInterface::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot
         coppeliasim_simulating_ = false;
         return false;
     }
+
+    tcp_transform_.header.frame_id = tf_prefix_ + "base";
+    tcp_transform_.child_frame_id = tf_prefix_ + "tool0_controller";
     
 
 
-
+    robot_nh.param<std::string>("wrench_frame_id", wrench_frame_id, "wrench");
     if(!this->root_nh.getParam("hardware_interface/joints", joint_names_)){
         ROS_ERROR_STREAM("Could not find joint-names in " << this->root_nh.resolveName("hardware_interface/joints"));
         return false;
@@ -59,7 +84,6 @@ bool CoppeliaSimInterface::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot
     {
         ROS_DEBUG_STREAM("Registering handles for joint " << joint_names_[i]);
         // Create joint state interface for all joints
-        ROS_ERROR_STREAM("Fully connected to CoppeliaSim!A");
         js_interface_.registerHandle(hardware_interface::JointStateHandle(joint_names_[i], &joint_positions_[i],
                                                                         &joint_velocities_[i], &joint_efforts_[i]));
 
@@ -85,8 +109,26 @@ bool CoppeliaSimInterface::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot
     speedsc_interface_.registerHandle(
         ur_controllers::SpeedScalingHandle("speed_scaling_factor", &speed_scaling_combined_));
 
-    // robot_status_interface_.registerHandle(industrial_robot_status_interface::IndustrialRobotStatusHandle(
-    //     "industrial_robot_status_handle", robot_status_resource_));
+    if(simxGetObjectHandle(client_id_, "tool0", &ft_sensor_hande_, simx_opmode_blocking) == simx_return_ok
+      && simxGetObjectHandle(client_id_, "TCP", &tcp_handle_, simx_opmode_blocking) == simx_return_ok
+      && simxGetObjectHandle(client_id_, "base", &base_handle_, simx_opmode_blocking) == simx_return_ok){
+        simxFloat dummy[4];
+        simxReadForceSensor(client_id_, ft_sensor_hande_, NULL, dummy, dummy, simx_opmode_streaming);
+        simxGetObjectPosition(client_id_, tcp_handle_, base_handle_, dummy, simx_opmode_streaming);
+        simxGetObjectQuaternion(client_id_, tcp_handle_, base_handle_, dummy, simx_opmode_streaming);
+        fts_interface_.registerHandle(hardware_interface::ForceTorqueSensorHandle(
+          wrench_frame_id, tf_prefix_ + "tool0_controller", &fts_measurements_[0], &fts_measurements_[3]));
+        registerInterface(&fts_interface_);
+        ROS_DEBUG_STREAM("Initialized tool-handling (force-torque-interface and TCP-pose)");
+      }
+    else{
+      ROS_ERROR_STREAM("Could not initialize fts-interface");
+      return false;
+    }
+    
+
+    robot_status_interface_.registerHandle(industrial_robot_status_interface::IndustrialRobotStatusHandle(
+        "industrial_robot_status_handle", robot_status_resource_));
     // Register interfaces
     registerInterface(&js_interface_);
     registerInterface(&spj_interface_);
@@ -94,7 +136,12 @@ bool CoppeliaSimInterface::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot
     registerInterface(&vj_interface_);
     registerInterface(&svj_interface_);
     registerInterface(&speedsc_interface_);
+    registerInterface(&robot_status_interface_);
+    tare_sensor_srv_ = robot_nh.advertiseService("zero_ftsensor", &CoppeliaSimInterface::zeroFTSensor, this);
+    tcp_pose_pub_.reset(new realtime_tools::RealtimePublisher<tf2_msgs::TFMessage>(root_nh, "/tf", 100));
     ROS_DEBUG_STREAM("CoppeliaSim hardware-interface has been initialized successfully!");
+    set_speed_slider_srv_ = robot_nh.advertiseService("set_speed_slider", &CoppeliaSimInterface::setSpeedSlider, this);
+    pause_button_srv_ = robot_nh.advertiseService("pause", &CoppeliaSimInterface::setPause, this);
     return true;
 }
 
@@ -105,8 +152,8 @@ bool CoppeliaSimInterface::advanceCoppeliaSim(){
 bool CoppeliaSimInterface::prepareSwitch(const std::list<hardware_interface::ControllerInfo>& start_list,
                                       const std::list<hardware_interface::ControllerInfo>& stop_list)
 {
-  bool ret_val = true;/*
-  if (controllers_initialized_ && !isRobotProgramRunning() && !start_list.empty())
+  bool ret_val = true;
+  if (controllers_initialized_ && !start_list.empty())
   {
     for (auto& controller : start_list)
     {
@@ -118,10 +165,39 @@ bool CoppeliaSimInterface::prepareSwitch(const std::list<hardware_interface::Con
         ret_val = false;
       }
     }
-  }*/
+  }
 
   controllers_initialized_ = true;
   return ret_val;
+}
+
+void CoppeliaSimInterface::extractToolPose(const ros::Time& timestamp)
+{
+
+  tf2::Quaternion rotation;
+  rotation.setX(tcp_pose_[3]);
+  rotation.setY(tcp_pose_[4]);
+  rotation.setZ(tcp_pose_[5]);
+  rotation.setW(tcp_pose_[6]);
+  tcp_transform_.header.stamp = timestamp;
+  tcp_transform_.transform.translation.x = tcp_pose_[0];
+  tcp_transform_.transform.translation.y = tcp_pose_[1];
+  tcp_transform_.transform.translation.z = tcp_pose_[2];
+
+  tcp_transform_.transform.rotation = tf2::toMsg(rotation);
+}
+
+void CoppeliaSimInterface::publishPose()
+{
+  if (tcp_pose_pub_)
+  {
+    if (tcp_pose_pub_->trylock())
+    {
+      tcp_pose_pub_->msg_.transforms.clear();
+      tcp_pose_pub_->msg_.transforms.push_back(tcp_transform_);
+      tcp_pose_pub_->unlockAndPublish();
+    }
+  }
 }
 
 void CoppeliaSimInterface::read(const ros::Time& time, const ros::Duration& period){
@@ -140,6 +216,57 @@ void CoppeliaSimInterface::read(const ros::Time& time, const ros::Duration& peri
         }
     
     }
+    simxFloat force_torque[6];
+    if(simxReadForceSensor(client_id_, ft_sensor_hande_, NULL, force_torque, force_torque + 3, simx_opmode_buffer) != simx_return_ok 
+        || simxGetObjectPosition(client_id_, tcp_handle_, base_handle_, tcp_pose_, simx_opmode_buffer) != simx_return_ok
+        || simxGetObjectQuaternion(client_id_, tcp_handle_, base_handle_, tcp_pose_ + 3, simx_opmode_buffer) != simx_return_ok){
+          ROS_ERROR_STREAM("Could not get TCP- or ForceTorqueSensor-Data");
+        }
+    else
+    {
+      fts_measurements_[0] = static_cast<double>(force_torque[0]);
+      fts_measurements_[1] = static_cast<double>(force_torque[1]);
+      fts_measurements_[2] = static_cast<double>(force_torque[2]);
+      fts_measurements_[3] = static_cast<double>(force_torque[3]);
+      fts_measurements_[4] = static_cast<double>(force_torque[4]);
+      fts_measurements_[5] = static_cast<double>(force_torque[5]);
+      extractToolPose(time);
+      publishPose();
+      std_msgs::Float64MultiArray out;
+
+      transformForceTorque();
+      out.data = fts_measurements_;
+      pub.publish(out);
+
+    }
+
+    if(run_state_ == RunState::PAUSED){
+      speed_scaling_combined_ = 0.0;
+    }
+    else if(run_state_ == RunState::RAMPUP){
+      speed_scaling_combined_ += pausing_ramp_up_increment_;
+      if(speed_scaling_combined_ >= speed_scaling_) run_state_ == RunState::RUNNING;
+    }
+    else{
+      speed_scaling_combined_ = speed_scaling_;
+    }
+    
+    
+
+}
+
+void CoppeliaSimInterface::transformForceTorque()
+{
+  tcp_force_.setValue(fts_measurements_[0], fts_measurements_[1], fts_measurements_[2]);
+  tcp_torque_.setValue(fts_measurements_[3], fts_measurements_[4], fts_measurements_[5]);
+
+  tf2::Quaternion rotation_quat;
+  tf2::fromMsg(tcp_transform_.transform.rotation, rotation_quat);
+  tcp_force_ = tf2::quatRotate(rotation_quat.inverse(), tcp_force_);
+  tcp_torque_ = tf2::quatRotate(rotation_quat.inverse(), tcp_torque_);
+
+  fts_measurements_ = { tcp_force_.x(),  tcp_force_.y(),  tcp_force_.z(),
+                        tcp_torque_.x(), tcp_torque_.y(), tcp_torque_.z() };
 }
 
 void CoppeliaSimInterface::write(const ros::Time& time, const ros::Duration& period){
@@ -239,5 +366,45 @@ void CoppeliaSimInterface::doSwitch(const std::list<hardware_interface::Controll
       ROS_WARN_STREAM("Controller switch stopped velocity- and position-control!");
   }
 }
+
+bool CoppeliaSimInterface::zeroFTSensor(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res)
+{
+  res.message = "CoppeliaSim has no zero-ing functionality for force-/torque-sensors";
+  res.success = false;
+  return true;
+}
+
+bool CoppeliaSimInterface::setSpeedSlider(ur_msgs::SetSpeedSliderFractionRequest& req,
+                                       ur_msgs::SetSpeedSliderFractionResponse& res)
+{
+  if (req.speed_slider_fraction >= 0.01 && req.speed_slider_fraction <= 1.0)
+  {
+    speed_scaling_ = req.speed_slider_fraction;
+    res.success = true;
+  }
+  else
+  {
+    res.success = false;
+  }
+  return true;
+}
+
+bool CoppeliaSimInterface::setPause(std_srvs::SetBoolRequest& req, std_srvs::SetBoolResponse& res){
+  if(run_state_ == RunState::RAMPUP){
+    res.success = false;
+    res.message = "Currently ramping up - Try again later";
+  }
+  else if((run_state_ == RunState::PAUSED && !req.data) || (run_state_ == RunState::RUNNING && req.data)){
+    res.success = false;
+    res.message = "Requested mode is already set";
+  }
+  else{
+    run_state_ = req.data ? RunState::RAMPUP : RunState::PAUSED;
+    res.success = true;
+    res.message = "Changed to requested mode";
+  }
+}
+
+PLUGINLIB_EXPORT_CLASS(CoppeliaSimInterface, hardware_interface::RobotHW)
 
 
