4a5826ef6a7d536d2c95475c5c59fc16d3083596
Sven Becker <ujily@student.kit.edu>
2021-02-06 17:44:28 +0100

    Add comments on legacy code

--- coppeliasim_interface/include/laser_scanner.h
+++ coppeliasim_interface/include/laser_scanner.h
@@ -1,3 +1,10 @@
+/*
+    WARNING!
+    This is legacy code used in early development stages to implement laser-scanner-readouts of CoppeliaSim
+    and does not work with the current CoppeliaSim-ROS-Framework. To make this work again, check out older branch 
+    "custom_hardware_interface" on the ROS branch and an old branch "agiprobot_scene". However, using this code is highly NOT recommended!
+*/
+
 extern "C" {
    #include "extApi.h"
 }
@@ -13,24 +20,36 @@ namespace coppeliasim_interface{
         public:
             LaserScanner();
             ~LaserScanner();
-            bool init(ros::NodeHandle& nh_);
-            // bool set(simxInt resolution, simxFloat angle);
-            bool sense(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res);
+            // Initialize LaserScanner so that all required members are resolved and services/topics/... advertised
+            bool init(ros::NodeHandle& nh_);            
         private:
-            // void applyNoise();
+            // Pointer on read-in meausrements
             simxFloat* measurements;
+            // Performs one line scan and publishes perceived point-cloud
+            bool sense(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res);
+            // Members used to apply noise on the measurements
             std::default_random_engine noise_generator;
             std::normal_distribution<double> noise_distribution;
             double noise_intensity;
+
+            // To publish the measured point-cloud
             ros::Publisher measurement_pub;
+            // For interaction with the ROS ecosystem
             ros::NodeHandle nh;
+            // Number of points per scan
             simxInt resolution;
+            // Fan angle (= opening angle of the fan) of the laser-scanner
             simxFloat angle;
+            // Measured distances from the scanner-frame to the first object-hit (maximum index = resolution - 1)
             std::vector<double> distances;
+            // To send out the position of the scanner-frame
             tf2_ros::TransformBroadcaster tf_broad;
+            // CoppeliaSim-Connection variable
             simxInt client_id;
+            // Handle of the "dummy" scanner_focus in CoppeliaSim
             simxInt scanner_focus_handle;
             std::string world_frame_id, scanner_focus_frame_id;
+            // When called, the sense() method will be performed
             ros::ServiceServer sense_srv;
     };
 }
--- coppeliasim_interface/src/laser_scanner_node.cpp
+++ coppeliasim_interface/src/laser_scanner_node.cpp
@@ -1,3 +1,12 @@
+/*
+    WARNING!
+    This is legacy code used in early development stages to implement laser-scanner-readouts of CoppeliaSim
+    and does not work with the current CoppeliaSim-ROS-Framework. To make this work again, check out older branch 
+    "custom_hardware_interface" on the ROS branch and an old branch "agiprobot_scene". However, using this code is highly NOT recommended!
+*/
+
+
+
 #include "../include/laser_scanner.h"
 
 int main(int argc, char** argv){
