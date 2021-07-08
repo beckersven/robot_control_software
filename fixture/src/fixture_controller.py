27c781c3e96209b6ab943b813aa23dca4b848402
Jarrypho <jan-philipp.kaiser@live.de>
2021-07-08 16:10:43 +0200

    Use correct names in fixture control

--- fixture/src/fixture_controller.py
+++ fixture/src/fixture_controller.py
@@ -11,21 +11,36 @@ class FixtureController:
         rospy.init_node("fixture_controller")
         self.joint_state_pub = rospy.Publisher(name="/joint_states", data_class=JointState, queue_size=1,latch=True)
         if rospy.get_param(param_name="/execution_type", default="") == "real":
-            rospy.wait_for_service(service="/...", timeout=rospy.Duration(10.0))
-            self.set_IO_service = rospy.ServiceProxy(name="/...", service_class=SetIO, persistent=True)
+            rospy.wait_for_service(service="/ur_hardware_interface/set_io", timeout=rospy.Duration(10.0))
+            self.set_IO_service = rospy.ServiceProxy(name="/ur_hardware_interface/set_io", service_class=SetIO, persistent=True)
         else:
             self.set_IO_service = None
-        
         self.fixture_moving_mutex = Lock()
         self.fixture_status = "INIT"
         self.set_fixture_cb(SetBoolRequest(data=False))
         self.set_fixture_service = rospy.Service(name="/close_fixture", service_class=SetBool, handler=self.set_fixture_cb)
     
-    def close_fixture(self, invert_to_open, pub_frequency_hz=10, duration_s=5):
+    def close_fixture(self, invert_to_open, pub_frequency_hz=10, duration_s=3):
         rate = rospy.Rate(hz=pub_frequency_hz)
         if self.set_IO_service is not None:
-            rospy.logerr("NOT YET IMPLEMENTED!")
-            #self.set_IO_service.call(SetIORequest())
+            request_pin_0 = SetIORequest(fun=1, pin=0)
+            request_pin_1 = SetIORequest(fun=1, pin=1)
+            request_order = []
+            if invert_to_open:
+                request_pin_0.state = 0
+                request_pin_1.state = 1
+                request_order = [request_pin_0, request_pin_1]    
+            else:
+                request_pin_0.state = 1
+                request_pin_1.state = 0
+                request_order = [request_pin_1, request_pin_0]
+            for request in request_order:
+                try:
+                    if not self.set_IO_service.call(request).success:
+                        return False
+                except:
+                    return False
+                rospy.sleep(0.5)
         joint_state_msg = JointState()
         joint_state_msg.name = ["fixture_static_dynamic"]
         for i in range(duration_s * pub_frequency_hz + 1):
